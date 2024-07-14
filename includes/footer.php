66738813d14901348d80bab522bea40e69085e10
Mansi Bhootadi <116282152+mansibhootadi@users.noreply.github.com>
2024-07-14 19:01:07 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,57 @@
+<footer>
+    <div class="w3ls-footer-grids pt-sm-4 pt-3">
+      <div class="container py-xl-5 py-lg-3">
+        <div class="row">
+          <div class="col-md-4 w3l-footer">
+            <h2 class="mb-sm-3 mb-2">
+              <a href="index.php" class="text-blue font-italic font-weight-bold">
+                <span style="color:black">Hemo </span>Track 
+                <i class='fas fa-heartbeat'></i>
+              </a>
+            </h2>
+            <p style="color: black;">Thank you for considering to donate blood and help save lives. Your selfless act of donating blood could make all the difference to someone in need. For any queries or feedback, please don't hesitate to contact us.</p>
+          </div>
+          <div class="col-md-4 w3l-footer">          
+          <h3 class="mb-sm-3 mb-2 text-black">Quick Links</h3>
+            <div class="nav-w3-l">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="index.php" style="color: black;">Home</a>
+                </li>
+                <li class="mt-2">
+                  <a href="about.php" style="color: black;">About Us</a>
+                </li>
+                <li class="mt-2">
+                  <a href="contact.php" style="color: black;">Contact Us</a>
+                </li>
+            
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 w3l-footer">
+            <h3 class="mb-sm-3 mb-2 text-black">Other Links</h3>
+            <div class="nav-w3-l">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="donor-list.php" style="color: black;">Donor List</a>
+                </li>
+                <li class="mt-2">
+                  <a href="search-donor.php" style="color: black;">Search Donor</a>
+                </li>
+                <li class="mt-2">
+                  <a href="admin/index.php" style="color: black;">Admin</a>
+                </li>
+            
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="border-top mt-5 pt-lg-4 pt-3 pb-lg-0 pb-3 text-center">
+          <p class="copy-right-grids mt-lg-1" style="color: black;">©  HemoTrack
+           
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- //footer -->
